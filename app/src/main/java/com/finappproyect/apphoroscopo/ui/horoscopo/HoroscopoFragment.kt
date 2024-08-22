d6aaee538a30b78a7f4bd5cd442e37627ee0d254
LucasBarrasa <lucasbarrasa96@gmail.com>
2024-08-22 15:18:03 -0300

    Base practica. Creacion y configuracion viewModel, state flow, modelo signos

--- app/src/main/java/com/finappproyect/apphoroscopo/ui/horoscopo/HoroscopoFragment.kt
+++ app/src/main/java/com/finappproyect/apphoroscopo/ui/horoscopo/HoroscopoFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.finappproyect.apphoroscopo.databinding.FragmentHoroscopoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,8 +13,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HoroscopoFragment : Fragment() {
 
+
     private var _binding: FragmentHoroscopoBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+         
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
