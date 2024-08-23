93f4f4f02a8f3ed13f9ce1f0a51945d58398fe7f
LucasBarrasa <lucasbarrasa96@gmail.com>
2024-08-23 12:28:13 -0300

    Agregado de string y imagenes para los signos

--- app/src/main/java/com/finappproyect/apphoroscopo/ui/horoscopo/HoroscopoFragment.kt
+++ app/src/main/java/com/finappproyect/apphoroscopo/ui/horoscopo/HoroscopoFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import com.finappproyect.apphoroscopo.databinding.FragmentHoroscopoBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
@@ -29,6 +31,10 @@ class HoroscopoFragment : Fragment() {
     }
 
     private fun initUIState() {
+        lifecycleScope.launch {
+
+
+        }
          
     }
 
